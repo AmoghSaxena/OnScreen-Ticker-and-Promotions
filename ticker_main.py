b882a2a20317a19cff11e73043a5b9c4ec8f550b
amogh.saxena <welcome2@digivalet>
2022-05-30 18:57:52 +0530

    Removed Unused Comments

--- ticker_main.py
+++ ticker_main.py
@@ -5,58 +5,6 @@ import json
 import pygame
 from moviepy.editor import *
 
-#
-# def optinal_font_n_length():
-#     with open("ticker_setup.json", "r") as f:
-#         conf = json.load(f)
-#
-#     message = conf['optional_ticker_message']
-#     ############## LIST OF ENGLISH FONTS ################
-#     if conf['optional_ticker_font'] == "MyriadProFont":
-#         optinal_font_n_length.ticker_font_size = 40
-#         optinal_font_n_length.left_length = int(float(len(message) * optinal_font_n_length.ticker_font_size) / 2.8) * (-1)
-#         optinal_font_n_length.optional_ticker_hight = int(conf['resolution_height'] / 120)
-#
-#     elif conf['optional_ticker_font'] == "Ubuntu":
-#         optinal_font_n_length.ticker_font_size = 40
-#         optinal_font_n_length.left_length = int(float(len(message) * optinal_font_n_length.ticker_font_size) / 2.1) * (-1)
-#         optinal_font_n_length.optional_ticker_hight = int(conf['resolution_height'] / 548)
-#
-#     elif conf['optional_ticker_font'] == "TimesNewRoman":
-#         optinal_font_n_length.ticker_font_size = 40
-#         optinal_font_n_length.left_length = int(float(len(message) * optinal_font_n_length.ticker_font_size) / 2.3) * (-1)
-#         optinal_font_n_length.optional_ticker_hight = int(conf['resolution_height'] / 385)
-#     #####################################################
-#
-#     ############ LIST OF RUSSIAN FONTS #################
-#     if conf['optional_ticker_font'] == "NotoSansArabic":
-#         optinal_font_n_length.ticker_font_size = 20
-#         optinal_font_n_length.left_length = int(float(len(message) * optinal_font_n_length.ticker_font_size) / 1.5) * (-1)
-#         optinal_font_n_length.optional_ticker_hight = int(conf['resolution_height'] / 1000)
-#     ####################################################
-#
-#     ########### LIST OF FREE FONTS ###################
-#     if conf['optional_ticker_font'] == "FreeSans":
-#         optinal_font_n_length.ticker_font_size = 40
-#         optinal_font_n_length.left_length = int(float(len(message) * optinal_font_n_length.ticker_font_size) / 1.9) * (-1)
-#         optinal_font_n_length.optional_ticker_hight = int(conf['resolution_height'] / 500)
-#     ####################################################
-#
-#     ########### LIST OF CHINESE FONTS ###################
-#     if conf['optional_ticker_font'] == "ZCOOLQingKeHuangYou":
-#         optinal_font_n_length.ticker_font_size = 40
-#         optinal_font_n_length.left_length = int(float(len(message) * optinal_font_n_length.ticker_font_size) / 1.2) * (-1)
-#         optinal_font_n_length.optional_ticker_hight = int(conf['resolution_height'] / 500)
-#     ####################################################
-#
-#     ########### LIST OF JAPANESE FONTS ###################
-#     if conf['optional_ticker_font'] == "NotoSansJP":
-#         optinal_font_n_length.ticker_font_size = 30
-#         optinal_font_n_length.left_length = int(float(len(message) * optinal_font_n_length.ticker_font_size) / 1.2) * (-1)
-#         optinal_font_n_length.optional_ticker_hight = int(conf['resolution_height'] / 800)
-#     ####################################################
-#
-
 # Function -> Main Ticker on bottom screen.
 def ticker_main():
     with open("ticker_setup.json", "r") as f:
