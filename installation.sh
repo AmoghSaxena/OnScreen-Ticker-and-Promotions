b9d547fbdde74c4db3e843fbd768e73ae3fc1aa8
amogh.saxena <welcome2@digivalet>
2022-06-01 17:34:02 +0530

    Refresh installation.sh for installation process

--- installation.sh
+++ installation.sh
@@ -5,7 +5,7 @@ TMPE=$( realpath "$0"  )
 cd $(dirname "$TMPE") && cd ../
 
 echo "INSTALLING VIRTUAL ENV"
-sudo apt install -y virtualenv pkg-config libcairo2-dev
+sudo apt install -y virtualenv pkg-config python3-dev
 INSTALL_LOCATION_VENV=$(pwd)/venv
 echo $INSTALL_LOCATION_VENV
 
@@ -27,12 +27,10 @@ if [[ $? == 0 ]]; then
    echo "VIRTUAL ENV SET!"
    if [[ $(python -c 'import sys; print(sys.version_info[:][0])') > 2 ]]; then
 	  echo "PYTHON VERSION 3+ CONTINUE TO INSTALLING DEPENDECIES"
-	  sudo apt install -y python3-dev gcc libgirepository1.0-dev python3-gi gobject-introspection gir1.2-gtk-3.0
 	  echo "INSTALLING PYTHON MODULES"
 	  pip install pip --upgrade
 	  pip install pygame==2.1.0
 	  pip install moviepy==1.0.3
-	  pip install PyGObject==3.42.0
 	else
 	  echo "IMPROPER VERISON OF PYTHON FOUND IN VIRTUAL ENV"
   fi
