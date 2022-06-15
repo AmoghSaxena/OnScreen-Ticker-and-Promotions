7b1cb69b2930b4e372e96a387cffe757ac91bd49
amogh.saxena <welcome2@digivalet>
2022-06-15 13:07:49 +0530

    Working on Test for Center

--- ticker_gtk.py
+++ ticker_gtk.py
@@ -56,7 +56,7 @@ def static_ticker_center():
     font_size_for_center = int(conf['static_ticker_font_size'] / 2)
     print(font_size_for_center)
 
-    fonting = pygame.font.SysFont(conf['static_ticker_font'], 40)
+    fonting = pygame.font.SysFont(conf['static_ticker_font'], font_size_for_center)
     texting = fonting.render(conf['static_ticker_message'], 1, tuple(conf['static_ticker_font_color']))
 
     textpos = texting.get_rect(centerx=screen.get_width() / 2)
