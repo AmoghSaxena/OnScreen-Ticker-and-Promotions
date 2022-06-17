1c18d05dc4425bba09de6b81e2af3905ca900fc1
amogh.saxena <welcome2@digivalet>
2022-06-17 12:38:48 +0530

    Emergency Ticker

--- ticker_emergency.py
+++ ticker_emergency.py
@@ -10,13 +10,14 @@ with open("ticker_setup.json", "r") as f:
 
 
 def static_ticker():
+    background_color = [255,255,255]
     image = Image.open(f"""{conf['BASE_DIR']}/media/logo_gtk.png""")
     image = image.resize((conf["resolution_width"], conf["resolution_height"]), Image.ANTIALIAS)
     image.save(fp=f"""{conf['BASE_DIR']}/media/res_logo_gtk.png""")
 
     screen = pygame.display.set_mode((conf['resolution_width'], conf['resolution_height']))
     while True:
-        screen.fill(255,255,255)
+        screen.fill(tuple(background_color))
         picture = pygame.image.load(conf['BASE_DIR'] + '/media/res_logo_gtk.png')
         screen.blit(picture, (0, 0))
 
