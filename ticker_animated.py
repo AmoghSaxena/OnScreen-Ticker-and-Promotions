a0648820082f8b946c5a8afc91a305669187421b
amogh.saxena <welcome2@digivalet>
2022-06-17 12:02:37 +0530

    Animated Sides!

--- ticker_animated.py
+++ ticker_animated.py
@@ -31,6 +31,30 @@ def ticker_animated_left():
 
     pygame.quit()
 
+def ticker_animated_sides():
+    with open("ticker_setup.json", "r") as f:
+        conf = json.load(f)
+
+    size = int(conf['resolution_height']/4.9)
+
+    if conf['moving_ticker_localtion'] == "bottom-right":
+        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % ((conf['resolution_width'] - size), (conf['resolution_height'] - size))
+    elif conf['moving_ticker_localtion'] == "bottom-left":
+        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, (conf['resolution_height'] - size))
+    if conf['moving_ticker_localtion'] == "top-right":
+        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % ((conf['resolution_width'] - size), 0)
+    elif conf['moving_ticker_localtion'] == "top-left":
+        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
+
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, (conf['resolution_height'] - size)) # Size of animated ticker
+    clip = VideoFileClip('media/myvideo.mp4').resize((size, size)) # Size of animated Ticker
+
+    while True:
+        clip.preview()
+
+    pygame.quit()
+
+
 
 def ticker_animated_center():
     with open("ticker_setup.json", "r") as f:
@@ -54,9 +78,11 @@ def ticker_animated_center():
 
 with open("ticker_setup.json", "r") as f:
     conf = json.load(f)
-if conf['moving_ticker_localtion'] == "right":
-    ticker_animated_right()
-elif conf['moving_ticker_localtion'] == "left":
-    ticker_animated_left()
-elif conf['moving_ticker_localtion'] == "center":
+# if conf['moving_ticker_localtion'] == "right":
+#     ticker_animated_right()
+# elif conf['moving_ticker_localtion'] == "left":
+#     ticker_animated_left()
+if conf['moving_ticker_localtion'] == "center":
     ticker_animated_center()
+else:
+    ticker_animated_sides()
