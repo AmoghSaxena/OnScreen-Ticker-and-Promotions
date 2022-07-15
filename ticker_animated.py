4652424ad6619a27b42b168a8e9ad8dd4e32592c
amogh.saxena <welcome2@digivalet>
2022-07-15 12:23:52 +0530

    Fix major issue. Python was calling size before assigning the local variable

--- ticker_animated.py
+++ ticker_animated.py
@@ -3,34 +3,7 @@ import os
 import pygame
 from moviepy.editor import *
 
-# def ticker_animated_right():
-#     with open("ticker_setup.json", "r") as f:
-#         conf = json.load(f)
-#
-#     size = int(conf['resolution_height']/4.9)
-#
-#     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % ((conf['resolution_width'] - size), (conf['resolution_height'] - size)) # Size of animated ticker
-#     clip = VideoFileClip('media/myvideo.mp4').resize((size, size)) # Size of animated Ticker
-#
-#     while True:
-#         clip.preview()
-#
-#     pygame.quit()
-#
-# def ticker_animated_left():
-#     with open("ticker_setup.json", "r") as f:
-#         conf = json.load(f)
-#
-#     size = int(conf['resolution_height']/4.9)
-#
-#     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, (conf['resolution_height'] - size)) # Size of animated ticker
-#     clip = VideoFileClip('media/myvideo.mp4').resize((size, size)) # Size of animated Ticker
-#
-#     while True:
-#         clip.preview()
-#
-#     pygame.quit()
-#
+
 def ticker_animated_sides():
     with open("ticker_setup.json", "r") as f:
         conf = json.load(f)
@@ -59,6 +32,8 @@ def ticker_animated_center():
     with open("ticker_setup.json", "r") as f:
         conf = json.load(f)
 
+        global size
+
         if conf['moving_ticker_center_size'] == "normal":
             size = 2
         elif conf['moving_ticker_center_size'] == "small":
@@ -77,10 +52,7 @@ def ticker_animated_center():
 
 with open("ticker_setup.json", "r") as f:
     conf = json.load(f)
-# if conf['moving_ticker_localtion'] == "right":
-#     ticker_animated_right()
-# elif conf['moving_ticker_localtion'] == "left":
-#     ticker_animated_left()
+
 if conf['moving_ticker_localtion'] == "center":
     ticker_animated_center()
 else:
