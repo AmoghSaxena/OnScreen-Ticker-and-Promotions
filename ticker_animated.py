7797a5c04c598f6fedfe6f316ae219c13893aa7b
amogh.saxena <welcome2@digivalet>
2022-07-18 17:03:48 +0530

    Testing Video Resize!

--- ticker_animated.py
+++ ticker_animated.py
@@ -9,27 +9,27 @@ def ticker_animated_sides():
     with open("ticker_setup.json", "r") as f:
         conf = json.load(f)
 
-    # size = int(conf['resolution_height']/4.9)
-
-    # if conf['moving_ticker_localtion'] == "bottom-right":
-    #     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % ((conf['resolution_width'] - size), (conf['resolution_height'] - size))
-    # elif conf['moving_ticker_localtion'] == "bottom-left":
-    #     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, (conf['resolution_height'] - size))
-    # elif conf['moving_ticker_localtion'] == "top-right":
-    #     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % ((conf['resolution_width'] - size), 0)
-    # elif conf['moving_ticker_localtion'] == "top-left":
-    #     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
+    size = int(conf['resolution_height']/4.9)
 
     if conf['moving_ticker_localtion'] == "bottom-right":
-        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % ((conf['resolution_width']), (conf['resolution_height']))
+        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % ((conf['resolution_width'] - size), (conf['resolution_height'] - size))
     elif conf['moving_ticker_localtion'] == "bottom-left":
-        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, (conf['resolution_height']))
+        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, (conf['resolution_height'] - size))
     elif conf['moving_ticker_localtion'] == "top-right":
-        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % ((conf['resolution_width']), 0)
+        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % ((conf['resolution_width'] - size), 0)
     elif conf['moving_ticker_localtion'] == "top-left":
         os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
 
-    clip = VideoFileClip('media/myvideo.mp4').resize((50),(200))# Size of animated Ticker
+    # if conf['moving_ticker_localtion'] == "bottom-right":
+    #     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % ((conf['resolution_width']), (conf['resolution_height']))
+    # elif conf['moving_ticker_localtion'] == "bottom-left":
+    #     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, (conf['resolution_height']))
+    # elif conf['moving_ticker_localtion'] == "top-right":
+    #     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % ((conf['resolution_width']), 0)
+    # elif conf['moving_ticker_localtion'] == "top-left":
+    #     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
+
+    clip = VideoFileClip('media/myvideo.mp4').resize((size, size))# Size of animated Ticker
 
     while True:
         clip.preview()
