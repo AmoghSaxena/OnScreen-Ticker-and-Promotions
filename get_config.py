a50afd6d6da12bb827f06fbaa916970f58b2e264
amogh.saxena <welcome2@digivalet>
2022-07-20 15:06:37 +0530

    Changes The Get Config files.

--- get_config.py
+++ get_config.py
@@ -7,7 +7,7 @@ import subprocess
 print(sys.argv)
 try:
     command = [
-        f"curl -s --location --request GET 'https://{sys.argv[1]}/ticker-config-api/' --header 'Authorization: Basic {sys.argv[2]}' --form 'ticker_id={sys.argv[3]}' | jq > /opt/ticker/ticker/config.json"]
+        f"curl -s --location --request GET 'https://{sys.argv[1]}/ticker-config-api/' --header 'Authorization: Basic {sys.argv[2]}' --form 'ticker_id={sys.argv[3]}' | jq '.' > /home/guest/rexter/OnScreen-Ticker-and-Promotions/config.json"]
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = process.stderr.read()
     exitstatus = process.wait()
@@ -19,14 +19,14 @@ except Exception as e:
     print(e)
     exit()
 
-with open("/opt/ticker/ticker/config.json", "r") as f:
+with open("/home/guest/rexter/OnScreen-Ticker-and-Promotions/config.json", "r") as f:
     conf = json.load(f)
 
 CONFIG = conf
 
 try:
     if bool(CONFIG['static_ticker_logo_name']):
-        command = [f"wget -O /opt/ticker/ticker/media/logo_gtk.png {CONFIG['static_ticker_logo_name']}"]
+        command = [f"wget -O /home/guest/rexter/OnScreen-Ticker-and-Promotions/media/logo_gtk.png {CONFIG['static_ticker_logo_name']}"]
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output = process.stderr.read()
         exitstatus = process.wait()
@@ -40,7 +40,7 @@ except:
 
 try:
     if bool(CONFIG['main_ticker_logo_name']):
-        command = [f"wget -O /opt/ticker/ticker/media/logo.png {CONFIG['main_ticker_logo_name']}"]
+        command = [f"wget -O /home/guest/rexter/OnScreen-Ticker-and-Promotions/media/logo.png {CONFIG['main_ticker_logo_name']}"]
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output = process.stderr.read()
         exitstatus = process.wait()
@@ -55,7 +55,7 @@ except:
 
 try:
     if bool(CONFIG['moving_ticker_logo_name']):
-        command = [f"wget -O /opt/ticker/ticker/media/myvideo.mp4 {CONFIG['moving_ticker_logo_name']}"]
+        command = [f"wget -O /home/guest/rexter/OnScreen-Ticker-and-Promotions/media/myvideo.mp4 {CONFIG['moving_ticker_logo_name']}"]
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output = process.stderr.read()
         exitstatus = process.wait()
