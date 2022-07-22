46fee3ddcc65e997a3c5eb5011c9123de295eb9b
amogh.saxena <welcome2@digivalet>
2022-07-22 18:58:18 +0530

    Test Margin / Border with video

--- ticker_gtk.py
+++ ticker_gtk.py
@@ -78,7 +78,7 @@ def static_ticker_center():
     while True:
         screen.fill(tuple(conf['static_ticker_bgcolor']))
         picture = pygame.image.load(conf['BASE_DIR'] + '/media/res_logo_gtk.png')
-        screen.blit(texting, (tuple(textpos)[0],(int(conf['static_ticker_font_size'] * 3.5))))
+        screen.blit(texting, (tuple(textpos)[0],(int(conf['static_ticker_font_size'] * 3.7))))
         screen.blit(picture, (space_image, space_image))
 
         pygame.display.flip()
