a83c5f5cd68b09fb5241af5b5af83d92ae578b6d
amogh.saxena <welcome2@digivalet>
2022-07-30 12:48:35 +0530

    Created New Branch!

--- get_config.py
+++ get_config.py
@@ -8,19 +8,47 @@ import subprocess
 BASE_DIR = Path(__file__).resolve().parent
 print(f"POSITION : {BASE_DIR}")
 print(sys.argv)
+# try:
+#     command = [
+#         f"curl -s --location --request GET 'https://{sys.argv[1]}/ticker-config-api/' --header 'Authorization: Basic {sys.argv[2]}' --form 'ticker_id={sys.argv[3]}' | jq '.' > {BASE_DIR}/config.json"]
+#     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#     output = process.stderr.read()
+#     exitstatus = process.wait()
+#     if exitstatus == 0:
+#         pass
+#     else:
+#         print(output.decode('utf-8'))
+# except Exception as e:
+#     print(e)
+#     exit()
+
+
 try:
-    command = [
-        f"curl -s --location --request GET 'https://{sys.argv[1]}/ticker-config-api/' --header 'Authorization: Basic {sys.argv[2]}' --form 'ticker_id={sys.argv[3]}' | jq '.' > {BASE_DIR}/config.json"]
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output = process.stderr.read()
-    exitstatus = process.wait()
-    if exitstatus == 0:
-        pass
-    else:
-        print(output.decode('utf-8'))
+    headers = {
+        'Authorization': f'Basic {sys.argv[2]}',
+    }
+
+    files = {
+        'ticker_id': (None, sys.argv[3]),
+    }
+
+    response = requests.get(f'http://{sys.argv[1]}/ticker-config-api/', headers=headers, files=files)
+
+
+
+
+    print(type(response.json()))
+    print(json.dumps(response.json(),indent=3))
+
+    with open(f"{BASE_DIR}/config.json", "w") as outfile:
+        outfile.write(json.dumps(response.json(), indent=3))
+    print("Done")
 except Exception as e:
     print(e)
     exit()
+
+
+
 
 with open(f"{BASE_DIR}/config.json", "r") as f:
     conf = json.load(f)
