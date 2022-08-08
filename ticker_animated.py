a72fe0de372b3d434990765f10c4267cf22c8fef
amogh.saxena <welcome2@digivalet>
2022-08-08 15:42:55 +0530

    Modified Video Settings

--- ticker_animated.py
+++ ticker_animated.py
@@ -30,9 +30,15 @@ elif conf['moving_ticker_localtion'] == "top-right":
     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % ((conf['resolution_width'] - (ratio * size)) - 20, 0)
 elif conf['moving_ticker_localtion'] == "top-left":
     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
+elif conf['moving_ticker_localtion'] == "top_fix_width":
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
+
+# CONFIG_DATA["resolution_width"], (CONFIG_DATA['resolution_height']) / 8
 
 if conf['moving_ticker_center_size'] == "full":
     clip = clip_video.resize((ratio * size, size)).without_audio()
+elif conf['moving_ticker_center_size'] == "full_width":
+    clip = clip_video.resize((conf["resolution_width"], (conf['resolution_height']) / 8)).margin(mar=10, color=(tuple(conf['moving_ticker_color']))).without_audio()
 else:
     clip = clip_video.resize((ratio * size, size)).margin(mar=10, color=(tuple(conf['moving_ticker_color']))).without_audio()
 
