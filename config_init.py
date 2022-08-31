d79da7f558d01f71845a5c9bb84ac81f29aa7510
amogh.saxena <welcome2@digivalet>
2022-08-31 12:47:29 +0530

    Adding Font Space for arabic

--- config_init.py
+++ config_init.py
@@ -120,7 +120,7 @@ def font_n_length(font_name):
     elif font_name == "russian":
         font_n_length.left_length = int(float(len(message) * font_n_length.ticker_font_size) / 1.9) * (-1)
     elif font_name == "arabic":
-        font_n_length.left_length = int(float(len(message) * font_n_length.ticker_font_size) / 1.85) * (-1)
+        font_n_length.left_length = int(float(len(message) * font_n_length.ticker_font_size) / 1.82) * (-1)
 
 
 def optinal_font_n_length(font_name):
